fdac5d6e01c8d261966c4d09f4fbaed1500eec59
Rogalowski <rogalowski@gmail.com>
2021-09-18 08:58:51 +0200

    Tests still InProgress

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from django.utils import timezone
 
-from ticket_app.models import Ticket, Department, User, Correspondence
+from ticket_app.models import Ticket, Department, User, Correspondence, DepartmentProblem
 
 
 #Plik conftest fixtury - globalnie widoczne wszedzie
@@ -15,7 +15,7 @@ def department():
 
 @pytest.fixture
 def department_problem():
-    return Department.objects.create(
+    return DepartmentProblem.objects.create(
         department_id=1,
         category_problem='Desktops/Software'
     )
@@ -25,6 +25,7 @@ def department_problem():
 def user():
     return User.objects.create(
         username='jacek',
+        password='jacek',
         first_name='Jacek',
         last_name='Rogowski',
         email='jacek@jacek.com',
@@ -33,15 +34,15 @@ def user():
         date_joined=timezone.now(),
         address_city='Szczecin',
         phone_number='111222333',
-        department_id=1,
+        department_id=1
     )
 
 
 @pytest.fixture
 def ticket():
-    ticket_create = Ticket.objects.create(
-        title='fake_title',
-        description='fake_description',
+    return Ticket.objects.create(
+        title='fake_title1',
+        description='fake_description1',
         status='Not Acknowledged',
         priorytet='Low',
         date_creation=timezone.now(),
@@ -49,11 +50,37 @@ def ticket():
         date_resolve=timezone.now(),
         department_assignment_id=1,
         problem_category_id=1,
-        user_requestor_id=1,
-        # user_assignment_id=1,
+        ticket_user_requestor=1
+        # user_requestor_id=1,
+        # user_requestor=User.objects.get(username='jacek'),
+        # user_assignment=1,
     )
-    ticket_create.user_assignment.add(1)
-    return ticket_create
+    # ticket_create.user_assignment.add(1)
+
+
+
+@pytest.fixture
+def ticket_user_assignment():
+    # ticket = Ticket.objects.get(title='fake_title')
+    ticket = Ticket.objects.create(
+        title='fake_title2',
+        description='fake_description2',
+        status='Not Acknowledged',
+        priorytet='Low',
+        date_creation=timezone.now(),
+        date_update=timezone.now(),
+        date_resolve=timezone.now(),
+        department_assignment_id=2,
+        problem_category_id=2,
+        user_requestor_id=2,
+        # user_assignment=1,
+    )
+
+    ticket_usr_ass = Ticket.objects.create(user_assignment=ticket.user_assignment.set(1))
+    # user_assignment=User.objects.get(username='jacek').set(1),
+    # Ticket.objects.create(ticket_id=1, user_id=1)
+    # return ticket_usr_ass
+    return ticket.user_assignment.set(1)
 
 
 @pytest.fixture
--- ticket_app/tests/test_views.py
+++ ticket_app/tests/test_views.py
@@ -1,6 +1,8 @@
 import pytest
 from django.test import Client
-from ticket_app.models import Ticket, Department
+from django.utils import timezone
+
+from ticket_app.models import Ticket, Department, DepartmentProblem
 
 
 # client = Client()
@@ -18,15 +20,27 @@ def test_department_model(department):
 
 
 @pytest.mark.django_db
+def test_department_problem_model(department_problem):
+    assert DepartmentProblem.objects.first()
+    assert DepartmentProblem.objects.get(category_problem='Desktops/Software') == department_problem
+
+
+@pytest.mark.django_db
 def test_ticket_model(ticket):
     assert len(Ticket.objects.all()) == 1
-    assert Ticket.objects.get(title='fake_title') == ticket
+
+
+@pytest.mark.django_db
+def test_ticket_user_assignment_model(ticket):
+    assert len(Ticket.objects.all()) == 1
+    assert Ticket.objects.all().get(title='fake_title2') == ticket
+    assert Ticket.objects.get(user_assignment_id=1) == ticket
 
 
 @pytest.mark.django_db  # polaczenie z testową baza danych
-def test_ticket_detail(client, ticket):
-    response = client.get(f'/tickets/ALL/')
-    # response = client.get(f'/ticket/{ticket.pk}/')
+def test_ticket_detail(client, department):  # ,ticket):
+    response = client.get(f'/tickets/{department}/')
+    # response = client.get(f'/ticket/{ticket.id}/')
     assert response.status_code == 200
     for name_ctx in [
         'title',
@@ -36,9 +50,41 @@ def test_ticket_detail(client, ticket):
         'date_creation',
         'date_update',
         'date_resolve',
-        'department_assignment',
-        'problem_category',
-        'user_requestor',
-        'user_assignment',
+        'department_assignment_id',
+        'problem_category_id',
+        'user_requestor_id',
     ]:
-        assert response.context[name_ctx] == getattr(ticket, name_ctx)
+        assert response.context[name_ctx] == getattr(department, name_ctx)
+        # assert response.context[name_ctx] == getattr(ticket, name_ctx)
+
+    # class Ticket:
+    #     title = 'title'
+    #     description = 'desc'
+    #
+    # ticket = Ticket()
+    # description = getattr(ticket, 'description', 'title1') => 'title3'
+
+
+@pytest.mark.django_db
+def test_ticket_detail(client):
+    response = client.get(f'/login_home/')
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_ticket_create(client):
+    data = {
+        'title': 'created_title',
+        'description': 'created_description',
+        'status': 'Not Acknowledged',
+        'priorytet': 'Low',
+        'date_creation': timezone.now(),
+        'date_update': timezone.now(),
+        'date_resolve': timezone.now(),
+        'department_assignment_id': 1,
+        'problem_category_id': 1,
+        'ticket_user_requestor': 1,
+        'user_assignment': 1,
+    }
+    response = client.post(f'/create_ticket/', data)
+    assert response.status_code == 302
+    assert Ticket.objects.first().title == 'created_title'

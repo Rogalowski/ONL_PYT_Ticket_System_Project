380feb5d1b485c67d330369d88229702ce6b6623
Rogalowski <rogalowski@gmail.com>
2022-07-29 10:05:35 +0200

    volumes, user dockercompose dockerfile

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/mnt/WindowsDaneNTFS/Programowanie/ONL_PYT_Ticket_System_Project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
